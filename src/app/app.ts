85f2be82bd22b0b085338c0c66497e5f8688dbe7
ireneabm98 <ireneabmesc98@gmail.com>
2025-10-15 09:59:02 -0600

    Card componente hijo character-component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { Characters } from './components/characters/characters'; // <--- Importa el componente
+import { CharactersComponent } from './components/characters/characters';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Characters], 
+  imports: [CharactersComponent], 
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
 export class App {
-  title = 'Rick & Morty - Personajes';
+  title = 'Rick & Morty';
 }
 
 
--- src/app/components/characters/character.interface.ts
+++ src/app/components/characters/character.interface.ts
@@ -1,7 +1,7 @@
 export interface Character {
-   
     id: number;
     name: string;
+    status: string; // 👈 Agregamos esta línea
     species: string;
     gender: string;
     image: string;
